63bdfd1867cd89507cc9388a7041aa6322ddf63a
rootz491 <experimentyt1@gmail.com>
2021-08-04 20:42:04 +0530

    views + mongodb connection

--- app.js
+++ app.js
@@ -1,9 +1,25 @@
-import express from "express";
-
-
+const express = require("express");
+const mongoose = require("mongoose");
+const logger = require("morgan");
+require("dotenv").config();
 
 const app = express();
 
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(console.log("connected to database successfully"));
 
 
 app.use(express.json())
+app.use(logger("dev"));
+
+
+app.get('/', (_, res) => {
+    res.send("hello");
+})
+
+
+app.listen(process.env.PORT, _ => {
+    console.log("server is running on port " + process.env.PORT)
+})
